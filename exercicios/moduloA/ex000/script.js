49d353bec674a29e7e11b27472ddbe78e1e65518
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2022-12-22 13:23:16 -0300

    Updates

--- exercicios/moduloA/ex000/script.js
+++ exercicios/moduloA/ex000/script.js
@@ -1,18 +1,7 @@
-var nome = window.prompt("Olá! Escreva seu nome:")
-    window.alert(`Seja bem vindo a minha página de testes ${nome} :)`)
-var idade = number(window.prompt(`${nome} Digite usa idade por favor:`))
-var dife = idade - 16 
-var dife2 = 16 - idade
-var text = idade = 17
-var text = 'Você é mais velho que eu 1 ano :)'
+var nome = window.prompt(`Olá, Digite seu nome`)
+var n1 = Number(window.prompt(`Digite a sua Nota 1`))
+var n2 = Number(window.prompt(`Agora digite a sua nota 2`))
+var  tota = (n1 + n2)/2
 
+//var result = tota => 8.0 ?window.confirm(`Muito bem você passou de ano`):window.confirm('Você está de recuperação')
 
- idade >= 18 ?window.confirm(`Nossa você ja é de maior, ${nome} você tem ${dife} mais velho que eu :) ` ):window.confirm(`${nome} você não é de maior ainda, eu tambem não `)
-
-
-
-
-/* var test = idade <16  ? window.confirm(`${nome} eu sou mais velho que você ${dife2} Anos ;)`):wind */
-
-
-/* idade < 16 ?window.confirm(`É só uma brincadeira ${nome} mas eu sou mais velho que você ${dife2} Anos`):window.confirm(`muito bem, vamos para a próxima parte `) */
