182cc59f606669e4415e52ab199ea50f6f72ce4e
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-05 11:31:52 -0300

    Conclusão de um desafio / Novo desafio

--- desafios/d002/desafios.boas.vindas/script.js
+++ desafios/d002/desafios.boas.vindas/script.js
@@ -1,2 +1,9 @@
-var dv = document.getElementsByTagName('div')[0]
+var a = document.getElementById('lnk')
 
+a.addEventListener('click', clicou)
+
+function clicou(){
+   var nome = window.prompt('Qual o seu nome?')
+   var idade = window.prompt(`Olá! ${nome} Quantos anos Você tem?`)
+   window.alert(`Acabei de conhecer o ${nome}, que tem ${idade} de idade!`)
+}
