287d5d497d4f66dd3f87fb1ded5ca64b54501aa7
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-05 18:32:19 -0300

    Updates in desafios

--- desafios/d005/desafio.conversor.medidas/script.js
+++ desafios/d005/desafio.conversor.medidas/script.js
@@ -1,15 +1,16 @@
 var link = document.getElementById('lnk')
-link.addEventListener('click', clicou)
+link.addEventListener('mouseup', clicou)
 
-var p = document.getElementById('txt')
-pbold.addEventListener('click', clicar)
+var p = document.getElementsByTagName('p')[1]
+p.addEventListener('click', clicar)
 
 
 
 function clicou(){
-    var num = Number(window.prompt(`Digite uma distancia em metors (m)`))
+    var num = Number(window.prompt(`Digite uma distancia em metros (m)`))
 
-   
+    p.innerText = `agora ${num}`
+
     var km = num / 1000
     var hm = num / 100 
     var dm = num * 10
@@ -17,19 +18,21 @@ function clicou(){
     var cm = num * 10000
     var mm = num * 100000 
 
-    document.writeln(`${km} quilômetros (km)`)
-    document.writeln(`${hm} hectômetros (hm)`)
-    document.writeln(`${dm} decímetros (dm)`)
-    document.writeln(`${dam} decâmetros (dam)`)
-    document.writeln(`${cm} centímetros (cm)`)
-    document.writeln(`${mn} milímetros (mm)`)
+
+    
+    //document.writeln(`${km} quilômetros (km)`)
+    //document.writeln(`<p>${hm} hectômetros (hm)</p>`)
+    //document.writeln(`${dm} decímetros (dm)`)
+    //document.writeln(`${dam} decâmetros (dam)`)
+    //document.writeln(`${cm} centímetros (cm)`)
+    //document.writeln(`${mn} milímetros (mm)`)
 }
 
 
-function clicar(){
-    window.document.body `A distancia de  metros, corresponde a...`
+/* function clicar(){
+ 
 }
 
-
+ */
 
     
