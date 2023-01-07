87764c0f9109f77447fe29e6693e1fc073677aaa
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-07 17:16:47 -0300

    Update Null

--- desafios/d006/desafio.conversor.temperatura/script.js
+++ desafios/d006/desafio.conversor.temperatura/script.js
@@ -3,12 +3,13 @@ cnvrt.addEventListener('click', clicou)
 
 function clicou(){
     var res = document.getElementById('class')
-    var Clsu = Number.parseFloat(window.prompt('Digite uma temperatura em °C (Celsius)'))
+    var Clsu = window.Number(prompt('Digite uma temperatura em °C (Celsius)'))
     //var vlr = Number(Clsu.value)
     var klvn = Clsu + 273
     var Fhrein = Clsu * 1.8 + 32
 
-    klvn.toFixed(2).replace('.',",")
+    //var ttkelvin = Number.parseFloat(klvn.toFixed(2))
+    //var ttfhrein = Number.parseFloat(Fhrein.toFixed(2))
 
     res.innerHTML = `<p><strong>A temperatura ${Clsu}, Corresponde a...</strong></p>`
     res.innerHTML += `<p>${klvn}°K (Kelvin)</p>`
