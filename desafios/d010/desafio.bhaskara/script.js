a3e8bc28b347ff9a560a151fe495f9dd9e6615f4
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-09 12:13:23 -0300

    Finishing Challenge

--- desafios/d010/desafio.bhaskara/script.js
+++ desafios/d010/desafio.bhaskara/script.js
@@ -1,8 +1,20 @@
+var ver = document.getElementById('ver')
+ver.addEventListener('click', clicou)
 
 
 
+function clicou(){
+    var res = document.getElementById('res')
+    var a = window.prompt('Digite o valor de a')
+    var b = window.prompt('Digite o valor de b')
+    var c = window.prompt('Digite o valor de c')
 
+    var delta = b ** 2 - 4 * a * c
 
+    res.innerHTML += `<p>A equação atual é  ${a}x<sup>2</sup> + ${b}x + ${c} = 0</p> `
+    res. innerHTML += `O cálculo realizado será &#x0394 = ${b}x<sup>2</sup> - 4 . ${a} . ${c}`
+    res.innerHTML += `<p>Delta é igual a <mark>  &#x0394 = ${delta} </mark></p> <br><hr><br>`
+}
 
 
 
