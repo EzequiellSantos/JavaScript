664634688ab9bc7d3b1816c4b7cad50f8b412af4
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-09 10:03:28 -0300

    New Challenge

--- desafios/d008/desafio.porcentagem/script.js
+++ desafios/d008/desafio.porcentagem/script.js
@@ -1,20 +1,23 @@
-var pct = document.getElementById('pct')
-pct.addEventListener('click', clicou)
+var evnt = document.getElementById('pct')
+evnt.addEventListener('click', clicou)
 
 function clicou(){
+    var pct = window.prompt('Digite a porcentagem de Desconto')
     var res = document.getElementById('res')
-    var produto = window.prompt('Digite o Nome do produto que voçe deseja comprar')
-    var valor = window.prompt(`Quanto custa ${produto} que você qeur comprar?`)
+    //var produto = window.prompt('Digite o Nome do produto que voçe deseja comprar')
+    var valor = window.prompt(`Quanto custa o que você qeur comprar?`)
 
+    var por  = valor * pct / 100
+    var ttpor = String(por.toFixed(2).replace('.',',')) 
 
-    var por  = valor * 10 / 100
-    //var total = toString(por.toFixed(2))
-    var fmrt = parseFloat(por.toFixed(2)) 
-    var fin = valor - fmrt
+    var fin = valor - por
+    var ttfin = String(fin.toFixed(2).replace('.',','))
+    
+
 
 
 
     res.innerHTML += `<p>O valor inicial do seu produto é <strong>${valor} R$</strong></p>`
-    res.innerHTML += `E <strong>10%</strong> de <strong>${valor} R$</strong> é igual <strong>${por} R$</strong>`
-    res.innerHTML += `<p>O seu produto com o Desconto de <strong>10%</strong> fica <strong>${fin} R$</strong></p>`
+    res.innerHTML += `E <strong>${pct}%</strong> de <strong>${valor} R$</strong> é igual <strong>${ttpor} R$</strong>`
+    res.innerHTML += `<p>O seu produto com o Desconto de <strong>10%</strong> fica <strong>${ttfin} R$</strong></p><hr>`
 }
