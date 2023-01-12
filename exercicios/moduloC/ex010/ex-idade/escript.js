d97edfc1f7888e46d1c2077a5b8d25e59701a021
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-11 22:13:13 -0300

    New Challenge

---
+++ exercicios/moduloC/ex010/ex-idade/escript.js
@@ -0,0 +1,30 @@
+
+var nas = document.getElementById('nas-div')
+var sex = document.getElementById('sex')
+var ver = document.getElementById('ver')
+var ima = document.getElementById('ima')
+var img = document.getElementsByTagName('section')[0]
+
+ver.addEventListener('click', clicou)
+
+
+ function clicou(){
+    const imaidosa = document.getElementById('ima');
+
+    if (nas <= 1972 &&  sex == 'fem' ) {
+
+    /* const image = new Image()*/
+    ima.src += 'https://github.com/EzequiellSantos/JavaScript/blob/main/exercicios/moduloC/ex009/ex.idade/idosa.perfil.jpg?raw=true';
+
+    imaidosa.appendChild(ima);
+        
+    }else if (nas <= 1972 && sex =='mas') {
+       ima.src += 'https://github.com/EzequiellSantos/JavaScript/blob/main/exercicios/moduloC/ex009/ex.idade/idoso.perfil.jpg?raw=true' 
+
+       imaidosa.appendChild(ima);
+    }else {
+        ima.src = 'https://github.com/EzequiellSantos/JavaScript/blob/main/exercicios/moduloC/ex009/ex.idade/homem.perfil.jpg?raw=true'
+
+        imaidosa.appendChild(ima)
+    }
+} 
