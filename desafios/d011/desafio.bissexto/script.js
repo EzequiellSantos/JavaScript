3e7ae6d0aa82c12a6729b259bae647f5baf933c1
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-13 16:45:37 -0300

    Updates in Challenges

--- desafios/d011/desafio.bissexto/script.js
+++ desafios/d011/desafio.bissexto/script.js
@@ -14,3 +14,28 @@ var bubls = document.getElementsByClassName("bubl");
 for (var i = 0; i < bubls.length; i++) {
     bubls[i].addEventListener("click", animateButton, false);
 }
+
+var ver = document.getElementById('ver')
+var res = document.getElementById('res')
+
+ver.addEventListener('click', clicou)
+
+function clicou(){
+    var ano = window.prompt('Qual é o ano que você quer verificar?')
+
+    var anodays = Number.parseInt(ano / 4 )
+
+    //window.alert(typeof(anodays))
+    var calc = anodays / 4
+    var result = Number.parseInt(calc)
+
+    var bix = new Date()
+    var day = bix.getUTCDay()
+
+    window.alert(`${day}`)
+   /*  if(day == '366'){
+        res.innerText = "é biseexto"
+    }else{
+     res.innerText = 'não é bissexto'
+    } */
+} 
