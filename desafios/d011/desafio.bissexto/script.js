70471ab1316d894279a4f5ddea45954ebca405bb
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-13 10:26:14 -0300

    New Challenge

---
+++ desafios/d011/desafio.bissexto/script.js
@@ -0,0 +1,16 @@
+var animateButton = function (e) {
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove("animate");
+
+    e.target.classList.add("animate");
+    setTimeout(function () {
+        e.target.classList.remove("animate");
+    }, 700);
+};
+
+var bubls = document.getElementsByClassName("bubl");
+
+for (var i = 0; i < bubls.length; i++) {
+    bubls[i].addEventListener("click", animateButton, false);
+}
