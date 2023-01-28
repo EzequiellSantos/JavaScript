c23ae3cc4c50d19f9da7f121883eb70fd0410aaa
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-28 11:38:28 -0300

    Update script.js

--- exercicios/moduloE/exercícios/script.js
+++ exercicios/moduloE/exercícios/script.js
@@ -1,29 +1,29 @@
-
-
+var num = Number(document.getElementById('num').value)
+var res = document.getElementById('res')
+var res_2 = document.getElementById('res_2')
+var arry = []
 
 function adicionou (){
-    var res = document.getElementById('res')
-    var num = Number(document.getElementById('num').value)
-     
-    arry.push(num)
-
-    res.innerHTML += `Valor ${num} adicionado <br>`
     
+    arry.push(num)
+    res.innerHTML += `Valor ${num} adicionado <br>`
+
 }
  
- var arry = []
 
- 
 
 function finalizou (){
-    var res_2 = document.getElementById('res_2') 
-    res_2.innerHTML = ''
-    res_2.innerHTML += `<p>Ao todo temos ${arry.length} Número Cadastrados </p>`
-    res_2.innerHTML +=`<p>${arry.sort()}</p>`
-    res_2.innerHTML += `<p>O Menor Número é o ${arry.at(0)}</p>`
-    res_2.innerHTML += `<p>O Maior Número é o ${arry.at(-1)}</p>`
-    res_2.innerHTML += `<p> ${arry} </p>`
+    if(num === 0 ){
+       alert('Adicione Números')
+    }else{
     
+        res_2.innerHTML = ''
+        res_2.innerHTML += `<p>Ao todo temos ${arry.length} Número Cadastrados </p>`
+        res_2.innerHTML +=`<p>${arry.sort()}</p>`
+        res_2.innerHTML += `<p>O Menor Número é o ${arry.at(0)}</p>`
+        res_2.innerHTML += `<p>O Maior Número é o ${arry.at(-1)}</p>`
+
+    }
 } 
 
 // https://pt.stackoverflow.com/questions/549721/como-somar-medidas-de-um-array
