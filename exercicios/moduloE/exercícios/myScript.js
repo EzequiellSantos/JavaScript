9ba12a8d0cb41d065e594ca3a03eb172a9b32859
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-28 21:45:05 -0300

    Update in myscript.js

--- exercicios/moduloE/exercícios/myScript.js
+++ exercicios/moduloE/exercícios/myScript.js
@@ -1,25 +1,32 @@
-var num = Number(document.getElementById('num').value)
+
 var res = document.getElementById('res')
 var res_2 = document.getElementById('res_2')
 var arry = []
 
 function adicionou (){
-    
+    var num = Number(document.getElementById('num').value)
+
+    if(num >= 1 && num <=100){
     arry.push(num)
     res.innerHTML += `Valor ${num} adicionado <br>`
-
+    } else{
+        alert('DIgite um Número acima de 0')
+    }
 }
  
 
 
 function finalizou (){
+    var num = Number(document.getElementById('num').value)
     if(num === 0 ){
        alert('Adicione Números')
     }else{
     
         res_2.innerHTML = ''
         res_2.innerHTML += `<p>Ao todo temos ${arry.length} Número Cadastrados </p>`
-        res_2.innerHTML +=`<p>${arry.sort()}</p>`
+
+        arry.sort()
+        
         res_2.innerHTML += `<p>O Menor Número é o ${arry.at(0)}</p>`
         res_2.innerHTML += `<p>O Maior Número é o ${arry.at(-1)}</p>`
 
