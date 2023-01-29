8c111867f3b3e44fe29a3c0c1c3bde7f11a48d8c
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-29 11:35:25 -0300

    Finishing Ex :sheep:

--- exercicios/moduloE/exercícios/myScript.js
+++ exercicios/moduloE/exercícios/myScript.js
@@ -23,29 +23,39 @@ function finalizou (){
     if(arry.length == 0 ){
        alert('Adicione Números')
     }else{
+        let tot = arry.length
+        let maior = arry[0]
+        let menor = arry[0]
         let soma = 0
         let media = 0
         
         for(let pos in arry){
             soma += arry[pos]
-            media += arry[pos] / arry.length
+            media += soma / tot
+            if(arry[pos] > maior)
+                maior = arry[pos]
+            
+            if(arry[pos] < menor)
+                menor = arry[pos]
+        
         }
-
+        
         let mediatt = Number(media.toFixed(2))
 
         res_2.innerHTML = ''
-        res_2.innerHTML += `<p>Ao todo temos ${arry.length} Número Cadastrados </p>`
+        res_2.innerHTML += `<p>Ao todo temos ${tot} Número Cadastrados </p>`
 
         arry.sort()
 
-        res_2.innerHTML += `<p>O Menor Número é o ${arry.at(0)}</p>`
-        res_2.innerHTML += `<p>O Maior Número é o ${arry.at(-1)}</p>`
+        res_2.innerHTML += `<p>O Menor Número é o ${menor}</p>`
+        res_2.innerHTML += `<p>O Maior Número é o ${maior}</p>`
         
 
         res_2.innerHTML += `<p>A Soma dos Valores é ${soma} </p>`
         res_2.innerHTML += `<p>A Média dos Valores é ${mediatt}</p>`
 
     }
+    
 } 
 
 // https://pt.stackoverflow.com/questions/549721/como-somar-medidas-de-um-array
