4fe9958c76420c1ffdd3a61bcbf935116cc7086e
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2023-07-13 17:55:54 -0300

    Update Menu | Fix :bug:

--- exercicios/moduloE/style-menu/menu.js
+++ exercicios/moduloE/style-menu/menu.js
@@ -2,13 +2,11 @@ var menu = document.getElementById('menu');
 var menuImg = document.getElementById('img-menu')
 var homeImg = document.getElementById('img-home');
 
-
-
 menuImg.addEventListener('click', function() {
     menu.classList.add('actived')
     homeImg.style.display = 'block'
     menu.style.transition = 'none'
-})
+}) //para menus hambuger
 
 document.addEventListener('click', function(evento) {
     const clicadoDentroDoElemento = menu.contains(evento.target)
@@ -18,4 +16,4 @@ document.addEventListener('click', function(evento) {
       menu.style.transition = 'all 0.6s ease-in-out'
       menu.classList.remove('actived')
     }
-})
+})  
