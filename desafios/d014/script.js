bae25fe7678032c8d0ec56aa92971189b40bdcec
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2023-08-13 15:01:34 -0300

    Update in percent | Feat :flipper:

--- desafios/d014/script.js
+++ desafios/d014/script.js
@@ -1,4 +1,26 @@
 var res = document.querySelector("#resultado");
+let ipor =  document.querySelector("#ipor");
+
+
+ipor.addEventListener('keydown', function(event){
+
+    if(event.keyCode === 13){//validação da tecla enter(código 13)
+
+        let iporcent =  Number(document.querySelector("#ipor").value);
+        let ipornumb =  Number(document.querySelector("#inum").value);
+        
+        
+        function calcPercent(numero, percent){
+
+            let result = numero * percent / 100
+            return result
+            
+        }
+
+        res.innerHTML = `<span>${calcPercent(ipornumb, iporcent).toFixed(2)}</span>`
+    }
+
+})
 
 function executCalc(){
 
