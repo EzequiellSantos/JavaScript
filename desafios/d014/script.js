7dd523759444ae7fbe8ce351d30cd8beaa33f387
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2023-08-15 21:12:09 -0300

    Update Percent Validation | Feat :cat:

--- desafios/d014/script.js
+++ desafios/d014/script.js
@@ -2,13 +2,49 @@ var res = document.querySelector("#resultado");
 let ipor =  document.querySelector("#ipor");
 
 
+
+
 ipor.addEventListener('keydown', function(event){
 
     if(event.keyCode === 13){//validação da tecla enter(código 13)
 
         let iporcent =  Number(document.querySelector("#ipor").value);
         let ipornumb =  Number(document.querySelector("#inum").value);
-        
+    
+        if(ipor.value.length == '' || inum.value.length == ''){
+
+            alert('Preencha os Campos corretamente:)')
+
+        }else if(iporcent >= 0 && iporcent <=200 ){      
+            
+            function calcPercent(numero, percent){
+    
+                let result = numero * percent / 100
+                return result
+                
+            }
+    
+            res.innerHTML = `<span>${calcPercent(ipornumb, iporcent).toFixed(2)}</span>`
+
+        }else{ 
+
+            alert("Use Números acima de Zero e pequenos Na Porcentagem")
+
+        }
+    }    
+})
+
+
+function executCalc(){
+
+    let iporcent =  Number(document.querySelector("#ipor").value);
+    let ipornumb =  Number(document.querySelector("#inum").value);
+
+    if(ipor.value.length == '' || inum.value.length == ''){
+
+        alert('Preencha os Campos corretamente:)')
+
+    }else if(iporcent >= 0 && iporcent <=200 ){      
         
         function calcPercent(numero, percent){
 
@@ -18,24 +54,12 @@ ipor.addEventListener('keydown', function(event){
         }
 
         res.innerHTML = `<span>${calcPercent(ipornumb, iporcent).toFixed(2)}</span>`
+
+    }else{ 
+
+        alert("Use Números acima de Zero e pequenos Na Porcentagem")
+
     }
-
-})
-
-function executCalc(){
-
-    let iporcent =  Number(document.querySelector("#ipor").value);
-    let ipornumb =  Number(document.querySelector("#inum").value);
-
-    function calcPercent(numero, percent){
-
-        let result = numero * percent / 100
-        return result
-        
-    }
-
-    res.innerHTML = `<span>${calcPercent(ipornumb, iporcent).toFixed(2)}</span>`
-    refresh.innerHTML = '<span class="material-symbols-outlined">refresh</span>'
     
 }
 
