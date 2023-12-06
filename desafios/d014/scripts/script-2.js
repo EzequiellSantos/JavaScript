542824aaccbd11d6bc1fd5cd65afc6d6d472aeb8
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2023-12-05 22:16:08 -0300

    Update Percent | New Doc :telescope:

---
+++ desafios/d014/scripts/script-2.js
@@ -0,0 +1,87 @@
+var res = document.querySelector("#resultado");
+var ivalue = document.querySelector('#ival');
+var iallval = document.querySelector("#iallVal");
+
+//valores de porcentagens (tecla enter)
+ivalue.addEventListener('keydown', function (event) {
+
+    if (event.keyCode === 13) {//validação da tecla enter(código 13)
+
+        let ivaluenum = Number(document.querySelector("#ival").value);
+        let iallValue = Number(document.querySelector("#iallVal").value);
+
+        if (ivalue.value.length == '' || iallval.value.length == '') {//validação
+
+            alert('Preencha os campos corretamentes :)')
+
+        } else if (ivalue <= iallValue || ivalue >= 0 || iallValue > 0) {
+
+            function calcPercent(todo, valor) {
+                let resultado = 100 / todo * valor
+                return resultado    
+            }
+
+            res.innerHTML = `<span>${calcPercent(iallValue, ivaluenum).toFixed(2)} %</span>`
+
+        } else if (ivaluenum > iallValue) {//validação
+
+            alert('Use Números Acima de Zero | O valor sempre tem que ser menor que o total')
+        }
+    }
+})
+
+
+//calculo botão (Porcentagens de Valores)
+
+function executCont() {
+
+    let iallValue = Number(document.querySelector('#iallVal').value);
+    let ivaluenum = Number(document.querySelector('#ival').value);
+
+    if (ivalue.value.length == '' || iallval.value.length == '') {//validação
+
+        alert('Preencha os campos corretamentes :)')
+
+    } else if (ivaluenum <= iallValue || ivaluenum >= 0 && iallValue > 0) {
+
+        function calcPercent(todo, valor) {
+            let resultado = 100 / todo * valor
+            return resultado
+        }
+
+        res.innerHTML = `<span>${calcPercent(iallValue, ivaluenum).toFixed(2)} %</span>`
+
+    } else {//validação
+
+        alert('Use Números Acima de Zero | O valor sempre tem que ser menor que o completo (100%)')
+
+    }
+
+}
+
+//reload
+let reload = document.getElementById('reload')
+
+reload.addEventListener('click', function () {
+    window.location.reload(true)
+})
+
+//menu
+const menuItems = document.querySelectorAll('.menu li');
+const highlightBar = document.querySelector('.highlight-bar');
+
+menuItems.forEach((item) => {
+    item.addEventListener('mouseover', () => {
+        highlightBar.style.display = 'block'
+        const itemPosition = item.getBoundingClientRect();
+        highlightBar.style.left = itemPosition.left + 'px';
+        highlightBar.style.width = itemPosition.width + 'px';
+    });
+
+    item.addEventListener('mouseout', () => {
+        const selectedItem = document.querySelector('.menu li:hover');
+        if (!selectedItem) {
+            highlightBar.style.width = '0';
+        }
+    });
+});
--- desafios/d014/scripts/script.js
+++ desafios/d014/scripts/script.js
@@ -1,75 +1,72 @@
 var res = document.querySelector("#resultado");
-let ipor =  document.querySelector("#ipor");
+var ipor = document.querySelector("#ipor");
 
-//valores de porcentagens
-ipor.addEventListener('keydown', function(event){
+//valores de porcentagens (tecla enter)
+ipor.addEventListener('keydown', function (event) {
 
-    if(event.keyCode === 13){//validação da tecla enter(código 13)
+    if (event.keyCode === 13) {//validação da tecla enter(código 13)
 
-        let iporcent =  Number(document.querySelector("#ipor").value);
-        let ipornumb =  Number(document.querySelector("#inum").value);
-    
-        if(ipor.value.length == '' || inum.value.length == ''){ //validação
+        let inum = Number(document.querySelector("inum").value)
+        let iporcent = Number(document.querySelector("#ipor").value);
+        let ipornumb = Number(document.querySelector("#inum").value);
+
+        if (ipor.value.length == '' || inum.length == '') { //validação
 
             alert('Preencha os Campos corretamente:)')
 
-        }else if(iporcent >= 0 && iporcent <=200 ){ //calculo (Valores de Porcentagens)
-            
-            function calcPercent(numero, percent){
-    
+        } else if (iporcent >= 0 && iporcent <= 200) { //calculo (Valores de Porcentagens)
+
+            function calcPercent(numero, percent) {
+
                 let result = numero * percent / 100
                 return result
-                
+
             }
-    
+
             res.innerHTML = `<span>${calcPercent(ipornumb, iporcent)}</span>`
 
-        }else{ //validação
+        } else { //validação
 
             alert("Use Números acima de Zero e pequenos Na Porcentagem")
 
         }
-    }    
+    }
 })
 
-//calculo porcentagens de valores
+// calculo por botão (Valores de Porcentagens)
 
+function executCalc() {
 
+    let iporcent = Number(document.querySelector("#ipor").value);
+    let ipornumb = Number(document.querySelector("#inum").value);
 
-// calculo por botão
-
-function executCalc(){
-    
-    let iporcent =  Number(document.querySelector("#ipor").value);
-    let ipornumb =  Number(document.querySelector("#inum").value);
-
-    if(ipor.value.length == '' || inum.value.length == ''){//validação
+    if (ipor.value.length == '' || inum.value.length == '') {//validação
 
         alert('Preencha os Campos corretamente :)')
 
-    }else if(iporcent >= 0 && iporcent <=200 ){      
-        
-        function calcPercent(numero, percent){
+    } else if (iporcent >= 0 && iporcent <= 200) {
+
+        function calcPercent(numero, percent) {
 
             let result = numero * percent / 100
             return result
-            
+
         }
 
         res.innerHTML = `<span>${calcPercent(ipornumb, iporcent)}</span>`
 
-    }else{ //validação
+    } else { //validação
 
         alert("Use Números acima de Zero e pequenos Na Porcentagem")
 
     }
-    
+
 }
 
 //reload
 let reload = document.getElementById('reload')
 
-reload.addEventListener('click', function(){
+reload.addEventListener('click', function () {
     window.location.reload(true)
 })
 
@@ -82,7 +79,7 @@ menuItems.forEach((item) => {
     item.addEventListener('mouseover', () => {
         highlightBar.style.display = 'block'
         const itemPosition = item.getBoundingClientRect();
-        highlightBar.style.left = itemPosition.left  + 'px';
+        highlightBar.style.left = itemPosition.left + 'px';
         highlightBar.style.width = itemPosition.width + 'px';
     });
 
