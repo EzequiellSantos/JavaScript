af56f0d038f0246d26d5e4bc323894ab52987a4b
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2023-12-14 22:07:54 -0300

    Update d014 | Feat :night_with_stars:

---
+++ desafios/d015/scripts/script.js
@@ -0,0 +1,12 @@
+var teste = document.getElementById("hora");
+
+function executando(){
+    teste.innerHTML += "<br> Hello <br>"
+}
+
+var intervaloFunction = setInterval(executando, 500)
+
+/* setTimeout(function (){ // cancelamento da função após 3 segundos atraves da função clearInterval
+    clearInterval(intervaloFunction);
+    teste.innerText = "repetição cancelada com 3 segundos"
+}, 3000) */
