e086cbb87628357bad28cde02c519c565846807d
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2023-12-14 22:37:12 -0300

    Update d010 | Feat :cactus:

--- desafios/d015/scripts/script.js
+++ desafios/d015/scripts/script.js
@@ -6,7 +6,7 @@ function executando(){
 
 var intervaloFunction = setInterval(executando, 500)
 
-/* setTimeout(function (){ // cancelamento da função após 3 segundos atraves da função clearInterval
+ setTimeout(function (){ // cancelamento da função após 3 segundos atraves da função clearInterval
     clearInterval(intervaloFunction);
-    teste.innerText = "repetição cancelada com 3 segundos"
-}, 3000) */
+    teste.innerText = "repetição cancelada com 30 segundos"
+}, 30000) 
