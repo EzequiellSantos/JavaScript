eb4fb433b15ad7b465b02d264585559da944d484
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-01-25 10:45:23 -0300

    Update Calc | Feat :memo:

---
+++ desafios/d015/scripts/calculadora.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const temaEstilos = document.getElementById('tema-estilos'); // estilos => style.css / animation.css
+    var toggleSection = document.getElementById("toggle-icon") // section que guarda lua | sol
+
+    const temaPreferido = localStorage.getItem('tema-preferido') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+
+    // Aplicação dos estilos iniciais
+    aplicarTema(temaPreferido);
+
+
+    // Mudando e Guardando o tema escolhido
+    toggleSection.onclick = () => {
+
+        let novoTema = temaEstilos.dataset.tema === 'light' ? 'dark' : 'light';
+        aplicarTema(novoTema);
+        localStorage.setItem('tema-preferido', novoTema); // guardando o tema escolhido  
+
+    };
+
+    function aplicarTema(tema) {
+
+        // Adicionar ou remover a classe conforme o tema
+        document.body.classList.remove('dark', 'light');
+        document.body.classList.add(`${tema}`);
+
+        // Atualizar o dataset para referência futura
+        temaEstilos.dataset.tema = tema;
+
+    }
+
+});
+
+//configurações da calculadora
