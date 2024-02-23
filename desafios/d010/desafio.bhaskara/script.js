8f23a7b65631698b143f2f5a1a7755483730eef5
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-02-23 14:31:46 -0300

    Update Bhaskara | Fix

--- desafios/d010/desafio.bhaskara/script.js
+++ desafios/d010/desafio.bhaskara/script.js
@@ -11,7 +11,7 @@ ver.addEventListener('click', clicou)
     var delta = b ** 2 - 4 * a * c //conta do delta
 
     res.innerHTML += `<p>A equação atual é  ${a}x<sup>2</sup> + ${b}x + ${c} = 0</p> `//numeros do delta
-    res. innerHTML += `O cálculo realizado será &#x0394 = ${b}x<sup>2</sup> - 4 . ${a} . ${c}`//calculo
+    res. innerHTML += `O cálculo realizado será &#x0394 = ${b}<sup>2</sup> - 4 . ${a} . ${c}`//calculo
     res.innerHTML += `<p>Delta é igual a <mark>  &#x0394 = ${delta} </mark></p> <br><hr><br>`//resposta do delta
 } 
 
