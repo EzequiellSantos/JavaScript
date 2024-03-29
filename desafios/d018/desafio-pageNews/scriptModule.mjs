e2ff37a409d59dc92724a323ad684d156b65aed8
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-03-29 13:58:27 -0300

    Remake Page | Build

--- desafios/d018/desafio-pageNews/scriptModule.mjs
+++
@@ -1,7 +0,0 @@
-// Exportando uma função
-export function somar(a, b) {
-  return a + b;
-}
-
-// Exportando uma variável
-export const PI = 3.14159265359;
--- desafios/d018/desafio-pageNews/scriptmain.mjs
+++
@@ -1,9 +0,0 @@
-// Importando a função somar e a constante PI do módulo contas
-import { somar, PI } from './scriptModule.mjs';
-
-
-// Usando a função e a constante
-console.log(somar(2, 3)); // Saída: 5
-
-resposta.innerHTML = `${somar(8, 3)}`
-console.log(PI); // Saída: 3.14159265359
