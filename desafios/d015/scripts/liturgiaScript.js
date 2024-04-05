fb9f6629b8a094aa6530268c43c3b81b015a2fa9
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-04-05 18:48:05 -0300

    Update Called API | Feat

--- desafios/d015/scripts/liturgiaScript.js
+++ desafios/d015/scripts/liturgiaScript.js
@@ -5,15 +5,13 @@ function adequarImagens(tema) {// adequa as imagens ao tema
 }
 
 var itensLi = document.querySelectorAll('#menuLeituras li')
-var firstLi = document.getElementById('liOneLeitura')
 var background = document.getElementById('background')
 var itemVar = ''
-
 
 itensLi.forEach((item) => {
 
     document.addEventListener('DOMContentLoaded', () => {
-        menuBackgroundInitial(firstLi)
+        menuBackgroundInitial(liFirstReading)
     })
 
     item.addEventListener('click', () => {
@@ -21,15 +19,13 @@ itensLi.forEach((item) => {
         itemVar = item
         menuBackground(item)
 
-
     })
-
 
 })
 
-let initiaLeft = firstLi.offsetLeft * 2.5
-function menuBackground(item){
+function menuBackground(item) {
 
+    let initiaLeft = background.offsetLeft
 
     background.style.left = initiaLeft < item.offsetLeft ? item.offsetLeft + 10 + 'px' : item.offsetLeft - 10 + 'px'
     background.style.width = item.offsetWidth * 1.08 + 'px'
@@ -37,11 +33,105 @@ function menuBackground(item){
 
 }
 
-function menuBackgroundDelay(){
+function menuBackgroundDelay() {
+
     background.style.left = itemVar.offsetLeft * 0.99 + 'px'
+
 }
 
-function menuBackgroundInitial(firstLi){
-    background.style.width = firstLi.offsetWidth + 'px'
-    background.style.left = firstLi.offsetLeft + 'px'
+function menuBackgroundInitial(liFirstReading) {
+
+    background.style.width = liFirstReading.offsetWidth + 'px'
+    background.style.left = liFirstReading.offsetLeft + 'px'
+
+}
+
+const url = 'https://intermediary-api.vercel.app/api'
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        coletarLeiturasAPI(data)
+        menuBackgroundInitial(liFirstReading)
+    })
+    .catch(error => {
+        console.error('Erro ao com a API; ', error);
+    })
+
+var leituras = {
+    firstLi: '',
+    salmos: '',
+    secondli: '',
+    evangelho: ''
+}
+
+function coletarLeiturasAPI(dados) {
+
+    // first reading
+    leituras.firstLi = dados.today.readings.first_reading.all_html
+
+    // salmos
+    leituras.salmos = dados.today.readings.psalm.all_html
+
+    // second reading
+    if (dados.hasOwnProperty('second_reading')) {
+
+        liSecondReading.style.display = 'block'
+        leituras.secondli = dados.today.readings.second_reading.all_html
+
+    } else {
+
+        ocultarSecondReading()
+        console.log('não ha segunda leitura');
+
+    }
+
+    // gospel
+    leituras.evangelho = dados.today.readings.gospel.all_html
+
+    // create first reading
+    gerarPrimeiraLeitura()
+
+}
+
+var containerTexto = document.getElementById('texto')
+
+var liSecondReading = document.getElementById('liTwoLeitura')
+liSecondReading.onclick = () => {
+
+    containerTexto.innerHTML = leituras.secondli
+
+}
+
+var liSalmos = document.getElementById('Salmos')
+liSalmos.onclick = () => {
+
+    containerTexto.innerHTML = leituras.salmos
+
+}
+
+var liEvangelho = document.getElementById('liEvangelho')
+liEvangelho.onclick = () => {
+
+    containerTexto.innerHTML = leituras.evangelho
+
+}
+
+var liFirstReading = document.getElementById('liOneLeitura')
+liFirstReading.onclick = () => {
+
+    gerarPrimeiraLeitura()
+
+}
+
+function gerarPrimeiraLeitura() {
+
+    containerTexto.innerHTML = leituras.firstLi
+
+}
+
+function ocultarSecondReading() {
+
+    liSecondReading.style.display = 'none'
+
 }
